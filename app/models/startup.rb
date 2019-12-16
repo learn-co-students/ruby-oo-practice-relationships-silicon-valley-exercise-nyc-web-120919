c8867c52b8ae13126bacd9424d325bd445a2589b
merewelch214 <merewelch@gmail.com>
2019-12-16 16:33:06 -0500

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,46 @@
 class Startup
 
+    attr_reader :name, :founder, :domain
+
+    @@all = []
+    
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+
+        @@all << self
+    end
+
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+        @self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(name)
+        @@all.find {|startup| startup.founder == name}
+    end
+
+    def sign_contract(vc, investment_type, invested_amt) 
+        FundingRound.new(self, vc, investment_type, invested_amt)
+    end
+
+    def num_funding_rounds
+        FundingRound.all.select {|round| round.startup == self}.length
+    end
+
+    def total_funds
+        startup_rounds = FundingRound.all.select {|round| round.startup == self}
+        investment_array = startup_rounds.map {|round| round.investment}
+        investment_array.reduce(0) {|sum,num| sum + num}
+    end
+
+    def self.domains
+        @@all.map {|startup| startup.domain}
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,14 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+su1 = Startup.new("LOAVES", "Paige Schmidt", "Food services")
+su2 = Startup.new("Brooklinen","Someone dooshy", "Home Goods") 
+su3 = Startup.new("Facebook","Mark Z", "Social Medias")
+su4 = Startup.new("The Happiest Kid Co","Karen Tilly", "Childcare")
+
+vc1 = VentureCapitalist.new("Doochebag", 2000000000)
+m= VentureCapitalist.new("Chad", 70000000)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
